7ed2af509ec2f426e6a9499d024c6b9c2b0661cd
lirongling <1843306093@qq.com>
2020-02-20 23:09:30 +0800

    rpx转换

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var  gulp = require('gulp');
+
+var  postcss = require('gulp-postcss');
+
+var  pxtorem = require('postcss-pxtransform');
+
+
+
+gulp.task('css', function() {
+
+
+
+    var processors = [
+
+        pxtorem({
+
+            platform: 'weapp',
+
+            designWidth: 750,
+
+        })
+
+    ];
+
+
+
+    return gulp.src(['miniprogram_npm/vant-weapp/**/*.wxss'])
+
+    .pipe(postcss(processors))
+
+    .pipe(gulp.dest('miniprogram_npm/vant-weapp/'));
+
+});
